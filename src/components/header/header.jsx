895f1f44a81a1337b9bcab5afa54eef2a358f609
Liri-39 <lirina74@gmail.com>
2021-07-02 15:43:40 +0200

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
 const Header = () => {
-    return <></>
+    return <header className="header">
+        <div className="header__wrapper">
+            <div className="header__logo logo">
+                <img src="/img/car.svg" alt="Логотип Avto-Moto"/>
+                <div className="logo__text">
+                    <span>Avto</span>
+                    <span>Moto</span>
+                </div>
+            </div>
+            <nav className="nav">
+                <ul className="nav__list">
+                    <li className="nav__item">
+                        <span>Автомобили</span>
+                    </li>
+                    <li className="nav__item">
+                        <span>Контакты</span>
+                    </li>
+                    <li className="nav__item">
+                        <span>Услуги</span>
+                    </li>
+                    <li className="nav__item">
+                        <span>Вакансии</span>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 };
 
 export default Header;
